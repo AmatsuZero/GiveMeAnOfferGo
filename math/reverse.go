05bf74b3d7ee12a73f3ac73ec1dce5f0541c7e6c
AmatsuZero <jzh16s@hotmail.com>
2019-08-24 21:19:23 +0800

    Initial commit

---
+++ math/reverse.go
@@ -0,0 +1,31 @@
+//https://leetcode-cn.com/problems/reverse-integer/description/
+
+package math
+
+import "math"
+
+func Reverse(x int) (result int) {
+	if (x >= 0 && x < 10) || (x < 0 && x > -10)  {
+		return x
+	}
+	stack := make([]int, 0)
+	left := x % 10
+	for left != 0 || x != 0 {
+		stack = append(stack, left)
+		x /= 10
+		left = x % 10
+	}
+	length := len(stack)
+	for i, num := range stack {
+		pos := 1
+		for index := 1; index < length - i; index++ {
+			pos *= 10
+		}
+		result += num * pos
+	}
+	// 边界检查
+	if result < math.MinInt32 || result > math.MaxInt32 {
+		result = 0
+	}
+	return
+}
---
+++ tests/twosum_test.go
@@ -0,0 +1,16 @@
+package tests
+
+import (
+	"../array"
+	"reflect"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T)  {
+	input := []int{2,7,11,15}
+	if reflect.DeepEqual(array.TwoSum(input, 9), []int{0,1}) {
+		t.Log("相等")
+	} else {
+		t.Error("不相等")
+	}
+}
