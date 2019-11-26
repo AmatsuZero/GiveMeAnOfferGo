6d72b190f81ed18cd9330b6701c04458870115d9
jiangzhenhua <jiangzhenhua@baidu.com>
2019-11-26 21:29:42 +0800

    ADD: 前缀树

---
+++ depth-first-search/MinimumLengthEncoding.go
@@ -0,0 +1,6 @@
+package depth_first_search
+
+func MinimumLengthEncoding(words []string) (length int) {
+
+	return
+}
---
+++ test/tree/Trie_test.go
@@ -0,0 +1,13 @@
+package tree
+
+import (
+	"testing"
+)
+
+func init() {
+
+}
+
+func TestTrieInsertString(t *testing.T) {
+
+}
