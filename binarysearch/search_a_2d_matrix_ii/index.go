e74d313fc322e411216574a0a4fc9d50d9d5af62
AmatsuZero <jzh16s@hotmail.com>
2022-03-31 20:34:14 +0800

    Add: more

---
+++ binarysearch/search_a_2d_matrix_ii/index.go
@@ -0,0 +1,20 @@
+package searcha2dmatrixii
+
+// https://leetcode-cn.com/problems/search-a-2d-matrix-ii/
+func SearchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) == 0 {
+		return false
+	}
+	row, col := 0, len(matrix[0])-1
+	for col >= 0 && row <= len(matrix)-1 {
+		if target == matrix[row][col] {
+			return true
+		} else if target > matrix[row][col] {
+			row++
+		} else {
+			col--
+		}
+
+	}
+	return false
+}
